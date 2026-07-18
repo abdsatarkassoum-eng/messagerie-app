e81e45fc358ee25d71b3878d057bf4641c7a110e
abdsatarkassoum-eng <abdsatarkassoum@gmail.com>
2026-07-18 08:18:21 +0100

    Add files via upload

---
+++ backend/src/models/CatalogItem.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const CatalogItem = sequelize.define(
+  'CatalogItem',
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    userId: { type: DataTypes.UUID, allowNull: false },
+    type: {
+      type: DataTypes.ENUM('product', 'service'),
+      defaultValue: 'product',
+    },
+    name: { type: DataTypes.STRING, allowNull: false },
+    description: { type: DataTypes.TEXT, allowNull: true },
+    price: { type: DataTypes.STRING, allowNull: true },
+    fileUrl: { type: DataTypes.STRING, allowNull: true },
+  },
+  {
+    tableName: 'catalog_items',
+    timestamps: true,
+  }
+);
+
+module.exports = CatalogItem;
