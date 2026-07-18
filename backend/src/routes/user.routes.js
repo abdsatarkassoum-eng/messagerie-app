257d77be4dbdaf499fafa29fd21552605b4c796a
abdsatarkassoum-eng <abdsatarkassoum@gmail.com>
2026-07-18 11:39:30 +0100

    Update user.routes.js

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -2,12 +2,20 @@
 const router = express.Router();
 const upload = require('../middleware/upload');
 const { auth } = require('../middleware/auth');
-const { searchUsers, getUser, updateProfile, getSuggestions, getUserProfile } = require('../controllers/user.controller');
+const {
+  searchUsers,
+  getUser,
+  updateProfile,
+  getSuggestions,
+  getUserProfile,
+  getUserFriendsList,
+} = require('../controllers/user.controller');
 
 router.get('/search', auth, searchUsers);
 router.get('/suggestions/list', auth, getSuggestions);
 router.put('/me', auth, upload.single('avatar'), updateProfile);
 router.get('/:id/profile', auth, getUserProfile);
+router.get('/:id/friends-list', auth, getUserFriendsList);
 router.get('/:id', auth, getUser);
 
 module.exports = router;
