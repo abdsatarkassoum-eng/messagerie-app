f4535fb26369459a90bd7aea36b8ff7f1173e3e9
abdsatarkassoum-eng <abdsatarkassoum@gmail.com>
2026-07-19 07:33:02 +0100

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,8 @@
-require('dotenv').config();
+ require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const compression = require('compression');
 const http = require('http');
 const { Server } = require('socket.io');
 
@@ -32,6 +33,7 @@ const io = new Server(server, {
 app.set('io', io);
 
 app.use(cors({ origin: CLIENT_URL, credentials: true }));
+app.use(compression());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.resolve(__dirname, '../uploads')));
