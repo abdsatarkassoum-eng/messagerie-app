c04ad3ba5a11897cfc4f628f0d2865db97edca08
abdsatarkassoum-eng <abdsatarkassoum@gmail.com>
2026-08-18 07:54:26 +0100

    Update hub.routes.js

--- backend/src/routes/hub.routes.js
+++ backend/src/routes/hub.routes.js
@@ -7,6 +7,7 @@ const {
   listSalons,
   createSalon,
   joinSalon,
+  getSalonSummary,
 } = require('../controllers/hub.controller');
 
 router.get('/', auth, listHubs);
@@ -14,5 +15,6 @@ router.get('/:hubId/categories', auth, listCategories);
 router.get('/categories/:categoryId/salons', auth, listSalons);
 router.post('/categories/:categoryId/salons', auth, createSalon);
 router.post('/salons/:salonId/join', auth, joinSalon);
+router.get('/salons/:salonId', auth, getSalonSummary);
 
 module.exports = router;
