c8c55b23fc66066d8be9db83338218fa7b8c7fac
abdsatarkassoum-eng <abdsatarkassoum@gmail.com>
2026-08-18 21:51:42 +0100

    Update catalog.routes.js

--- backend/src/routes/catalog.routes.js
+++ backend/src/routes/catalog.routes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const upload = require('../middleware/upload');
 const { auth } = require('../middleware/auth');
-const { listByUser, createItem, deleteItem } = require('../controllers/catalog.controller');
+const { listByUser, listMarketplace, createItem, deleteItem } = require('../controllers/catalog.controller');
 
+router.get('/', auth, listMarketplace);
 router.get('/user/:id', auth, listByUser);
 router.post('/', auth, upload.array('files', 6), createItem);
 router.delete('/:id', auth, deleteItem);
