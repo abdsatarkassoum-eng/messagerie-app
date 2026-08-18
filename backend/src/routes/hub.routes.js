bf305c0e3719333b068c6a4e29982692baf0f5b3
abdsatarkassoum-eng <abdsatarkassoum@gmail.com>
2026-08-18 07:20:52 +0100

    Create hub.routes.js

---
+++ backend/src/routes/hub.routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const { auth } = require('../middleware/auth');
+const {
+  listHubs,
+  listCategories,
+  listSalons,
+  createSalon,
+  joinSalon,
+} = require('../controllers/hub.controller');
+
+router.get('/', auth, listHubs);
+router.get('/:hubId/categories', auth, listCategories);
+router.get('/categories/:categoryId/salons', auth, listSalons);
+router.post('/categories/:categoryId/salons', auth, createSalon);
+router.post('/salons/:salonId/join', auth, joinSalon);
+
+module.exports = router;
